83714bb701a5234d27d635f498cc28106781c064
4Tipy <lchavez21@jaumebalmes.net>
2023-03-08 22:07:03 +0100

    practica acabada + video

--- resources/views/mensajeAdmin.blade.php
+++ resources/views/mensajeAdmin.blade.php
@@ -1,1 +1,3 @@
-{{$text}}
+<a href="{{ route('admin.clientes') }}"> {{$text}}Clientes</a> <br/>
+<a href="{{ route('admin.facturacion') }}">{{$text}}Facturacion</a> <br/>
+<a href="{{ route('admin.contabilidad') }}">{{$text}}Contabilidad</a>
